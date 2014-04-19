611f070daf40f728c56c397e98dc53e826e1b7f0
Alex <SkyIce39@gmail.com>
2014-04-20 01:20:59 +0300

    Update ShowMeCharge.lua

--- Scripts/ShowMeCharge.lua
+++ Scripts/ShowMeCharge.lua
@@ -1,58 +1,50 @@
 require("libs.Utils")
 
-charge = {}
-bara = false
+rect = {}
+sleeptick = 0
 
 function Tick(tick)
 
-	if not client.connected or client.loading or client.console or not entityList:GetMyHero() then return end
-	
-	me = entityList:GetMyHero()
-	
-	local hero = entityList:FindEntities({type=LuaEntity.TYPE_HERO,illusion=false,alive = true})
-		
-	for i,v in ipairs(hero) do local OnScreen = client:ScreenPosition(v.position)
+	if not client.connected or client.loading or client.console or tick < sleeptick then return end
 
-		if not bara then
-			if #hero == 10 then
-				allpick = true
-			end
-			if allpick then
-				if v.team ~= me.team then
-					if v.name ~= "npc_dota_hero_spirit_breaker" then				
-					script:Disable()					
-					else					
-					bara = true
-					end
-				end
-			end
-		end
-		
-		if OnScreen and v.team == me.team then
-			if charge[v.handle] ~= nil then
-				charge[v.handle]:SetVector(0, Vector(v.position.x, v.position.y, v.position.z+250) )
-			end		
-			if SleepCheck(v.handle) then
-				if v:DoesHaveModifier("modifier_spirit_breaker_charge_of_darkness_vision") then					
-					if charge[v.handle] == nil then
-						charge[v.handle] = Effect(Vector(v.position.x, v.position.y, v.position.z),"spirit_breaker_charge_target_mark")								
-					end
-				else
-					if not charge[v.handle] ~= nil then
-						charge[v.handle] = nil
-						collectgarbage("collect")						
-					end
-				end	Sleep(125,v.handle)
-			end
-		end
-	end
+	sleeptick = tick + 250
+
+	local me = entityList:GetMyHero()
 	
+	if not me then return end
+
+	local hero = entityList:FindEntities({type=LuaEntity.TYPE_HERO,illusion=false})
+
+	for i,v in ipairs(hero) do
+
+		if v.team == me.team then
+
+			local offset = v.healthbarOffset
+
+			if offset == -1 then return end
+
+			if not rect[v.handle] then 
+				rect[v.handle] = {}  rect[v.handle] = drawMgr:CreateRect(-10,-80,22,22,0xFF8AB160,drawMgr:GetTextureId("NyanUI/miniheroes/spiritbreaker")) rect[v.handle].visible = false 
+				rect[v.handle].entity = v rect[v.handle].entityPosition = Vector(0,0,offset)
+			end
+
+			if v.visible and v.alive then
+				if v:DoesHaveModifier("modifier_spirit_breaker_charge_of_darkness_vision") then					
+					rect[v.handle].visible = true
+				else
+					rect[v.handle].visible = false
+				end
+			else
+				rect[v.handle].visible = false
+			end
+		end
+
+	end
+
 end
 
 function GameClose()
-	charge = {}
-	bara = false
-	allpick = false
+	rect = {}	
 end
  
 script:RegisterEvent(EVENT_CLOSE,GameClose)
