76e5d3e4a7adadc75d084c5652de6507e83943dd
Alex <SkyIce39@gmail.com>
2014-04-30 21:32:58 +0300

    Update ShowMeCharge.lua

--- Scripts/ShowMeCharge.lua
+++ Scripts/ShowMeCharge.lua
@@ -1,7 +1,9 @@
 require("libs.Utils")
 
 rect = {}
+ch = {}
 sleeptick = 0
+start = true
 
 function Tick(tick)
 
@@ -13,23 +15,35 @@ function Tick(tick)
 	
 	if not me then return end
 
-	local hero = entityList:FindEntities({type=LuaEntity.TYPE_HERO,illusion=false})
+	local hero = entityList:FindEntities({type=LuaEntity.TYPE_HERO,alive=true,illusion=false})
 
 	for i,v in ipairs(hero) do
-
+	
+		if start and #hero == 10 then
+			if v.team ~= me.team then
+				table.insert(ch,v.name)
+				if #ch == 5 then
+					if ch[1] ~= "npc_dota_hero_spirit_breaker" and ch[2] ~= "npc_dota_hero_spirit_breaker" and ch[3] ~= "npc_dota_hero_spirit_breaker" and ch[4] ~= "npc_dota_hero_spirit_breaker" and ch[5] ~= "npc_dota_hero_spirit_breaker" then
+						script:Disable()
+					else
+						start = false
+					end
+				end
+			end			
+		end
+			
 		if v.team == me.team then
-
 			local offset = v.healthbarOffset
-
 			if offset == -1 then return end
 
 			if not rect[v.handle] then 
 				rect[v.handle] = {}  rect[v.handle] = drawMgr:CreateRect(-10,-80,22,22,0xFF8AB160,drawMgr:GetTextureId("NyanUI/miniheroes/spirit_breaker")) rect[v.handle].visible = false 
 				rect[v.handle].entity = v rect[v.handle].entityPosition = Vector(0,0,offset)
 			end
-
+			
 			if v.visible and v.alive then
-				if v:DoesHaveModifier("modifier_spirit_breaker_charge_of_darkness_vision") then					
+				if v:DoesHaveModifier("modifier_spirit_breaker_charge_of_darkness_vision") then	
+					print("123")
 					rect[v.handle].visible = true
 				else
 					rect[v.handle].visible = false
@@ -38,13 +52,15 @@ function Tick(tick)
 				rect[v.handle].visible = false
 			end
 		end
-
 	end
 
 end
 
 function GameClose()
 	rect = {}	
+	ch = {}
+	hero = nil
+	start = true
 end
  
 script:RegisterEvent(EVENT_CLOSE,GameClose)
