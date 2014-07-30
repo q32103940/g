baad35506a9b129fdaf209b64faa6d1041944e29
Inactive <SkyIce39@gmail.com>
2014-07-30 23:03:49 +0300

    Update Dota_Range_Display.lua

--- Scripts/Dota_Range_Display.lua
+++ Scripts/Dota_Range_Display.lua
@@ -7,144 +7,112 @@ config:Load()
 
 local sleep = 0
 local toggleKey = config.Hotkey
-local activated = false local move = false
-local xx = 180 local yy = 80
-local effect = {}
-local ranges = {}
-local range = {}
-local spellname = {}
-local spell = {}
-local img = {}
-local rect = {}
+local activated,move = false,false
+local xx,yy = 180,80
+local spellList = {}
+local spells = {}
 local myFont = drawMgr:CreateFont("manabarsFont","Arial",14,500)
 local text = drawMgr:CreateText(0,0,0xFFFFFFff,"Range Display",myFont)
 text.visible = false
 
-for a = 1, 7 do
-        img[a] = drawMgr:CreateRect(0,0,32,32,0x000000FF)
-        img[a].visible = false
-        rect[a] = drawMgr:CreateRect(0,0,36,36,0xFFFFFFff,true)
-        rect[a].visible = false
-        spellname[a] = false
-end
-
 function Tick(tick)
 
-        if not client.connected or client.loading or client.console or tick < sleep then return end
-		
-		sleep = tick + 300
+	if not client.connected or client.loading or client.console or tick < sleep then return end
+	
+	sleep = tick + 250
 
-		local me = entityList:GetMyHero()
+	local me = entityList:GetMyHero()
 
-		if not me then return end
-		
-		local ability = me.abilities
-		local num = rangelist[me.name]
-        for a,spells in ipairs(ability) do
-			if spells.name ~= "attribute_bonus" and not spells.hidden then
-				spell[a] = spells.name
-				if spells.level ~= 0 then
-					range[a] = spells.castRange
-					if rangelist[me.name] then
-						range[num.spell] = num.ran[me:GetAbility(num.spell).level]
-					end
-					if spellname[a] == true then
-						local dirty = false
-						if not effect[a] or ranges[a] ~= range[a] then
-								effect[a] = Effect(me,"range_display")
-								effect[a]:SetVector( 1,Vector(range[a],0,0) )
-								ranges[a] = range[a]
-								dirty = true
-						end
-						if dirty then
-								collectgarbage("collect")
-						end
-					else
-						local dirty = false
-						if effect[a] then
-								effect[a] = nil
-								dirty = true
-						end
-						if dirty then
-								collectgarbage("collect")
-						end
-					end
-				end
-			end
-        end
-
-end
-
-function Frame()
-
-	local count = #spell
-	if count == 0 then return end
-	if activated then vis = true
-		if xx == 180 and yy == 80 then LoadGUIConfig() end
-        if xx == nil and yy == nil then xx=180 yy = 80 end
-		if move == true then
-			xx = client.mouseScreenPosition.x - 39*#spell/2 - 20 yy = client.mouseScreenPosition.y + 15
-		end
-		text.visible = true
-		text.x = xx + 39*count/2
-		text.y = yy-18
-		for a = 1,count do
-			img[a].visible = true
-			if spell[a] == nil then
-				img[a].x = xx+38*a
-				img[a].y = yy
-				img[a].textureId = drawMgr:GetTextureId("NyanUI/spellicons/doom_bringer_empty1")
-			elseif spell[a] ~= nil then
-				img[a].x = xx+38*a
-				img[a].y = yy
-				img[a].textureId = drawMgr:GetTextureId("NyanUI/spellicons/"..spell[a])
-				if spellname[a] == true then
-					rect[a].visible = true
-					rect[a].x = xx-2+38*a
-					rect[a].y = yy-2
-					rect[a].color = 0xFFFFFFff
-				else
-					rect[a].visible = true
-					rect[a].x = xx-2+38*a
-					rect[a].y = yy-2
-					rect[a].color = 0x000000ff
-				end
-			end
-		end
-	else
-		if vis then
-			for a = 1,7 do
-					img[a].visible = false
-					rect[a].visible = false
-					text.visible = false
-			end
-			vis = false
+	if not me then return end
+	
+	local ability = me.abilities
+	local num = rangelist[me.name]
+	local spellList = {}
+	for a,spell in ipairs(ability) do
+		if spell.name ~= "attribute_bonus" and not spell.hidden then
+			table.insert(spellList,spell)
 		end
 	end
-end
+	global = #spellList
+	local count = #spellList
+	
+	if activated then
+		if xx == 180 and yy == 80 then LoadGUIConfig() end
+		if xx == nil and yy == nil then xx=180 yy = 80 end
+		if move == true then
+			xx,yy = client.mouseScreenPosition.x - 39*count/2 - 20,client.mouseScreenPosition.y + 15
+		end
+		text.x,text.y,text.visible = xx + 39*count/2,yy-18,true		
+		for a,v in ipairs(spellList) do
+			if not spells[a] then spells[a] = {} end
+			if not spells[a].img then
+				spells[a].img = drawMgr:CreateRect(0,0,32,32,0x000000FF) spells[a].img.visible = false
+				spells[a].rect = drawMgr:CreateRect(0,0,36,36,0xFFFFFFff,true) spells[a].rect.visible = false
+			end
+			spells[a].img.x,spells[a].img.y = xx+38*a,yy
+			spells[a].rect.x,spells[a].rect.y = xx+38*a-2,yy - 2
+			spells[a].img.textureId = drawMgr:GetTextureId("NyanUI/spellicons/"..v.name)
+			if spells[a].state then					
+				spells[a].rect.color = 0xFFFFFFff
+			else
+				spells[a].rect.color = 0x000000ff
+			end
+			spells[a].img.visible,spells[a].rect.visible = true,true
+		end
+	elseif text.visible then
+		for a,v in ipairs(spellList) do
+			spells[a].img.visible,spells[a].rect.visible = false,false		
+		end
+		text.visible = false
+	end
 
+	for a,v in ipairs(spellList) do
+		if v.level ~= 0 and #spells ~= 0 then
+			spells[a].range = v.castRange
+			if rangelist[me.name] then
+				spells[a].range = num.ran[me:GetAbility(num.spell).level]
+			end
+			local dirty = false
+			if spells[a].state then
+				if not spells[a].effect or spells[a].ranges ~= spells[a].range then
+					spells[a].effect = Effect(me,"range_display")
+					spells[a].effect:SetVector( 1,Vector(spells[a].range,0,0) )
+					spells[a].ranges = spells[a].range
+					dirty = true
+				end						
+			elseif spells[a].effect then
+				spells[a].effect = nil
+				dirty = true
+			end
+			if dirty then
+				collectgarbage("collect")
+			end
+		end
+	end
+
+end
 
 function Key(msg,code)
 
-	local count = #spell
+	local count = global
 
-	if count == 0 or client.chat then return end
+	if not count or client.chat then return end
 
 	if IsKeyDown(toggleKey) then
 		activated = not activated
 	end
 
 	if activated then
-		if IsMouseOnButton(xx+39*count/2,yy-20,20,100) then
-			if msg == LBUTTON_UP then
-					move = (not move)
-			end
-			SaveGUIConfig(xx,yy)
+		if msg == LBUTTON_UP then
+			if IsMouseOnButton(xx+39*count/2,yy-20,20,100) then
+				move = (not move)
+				SaveGUIConfig(xx,yy)
+			end			
 		end
-		for a = 1,count do
-			if IsMouseOnButton(xx+38*a,yy,32,32) then
-				if msg == LBUTTON_UP then
-					spellname[a] = (not spellname[a])
+		if msg == LBUTTON_UP then
+			for a = 1,count do
+				if IsMouseOnButton(xx+38*a,yy,32,32) then
+					spells[a].state = (not spells[a].state)
 				end
 			end
 		end
@@ -153,39 +121,37 @@ function Key(msg,code)
 end
 
 function IsMouseOnButton(x,y,h,w)
-        local mx = client.mouseScreenPosition.x
-        local my = client.mouseScreenPosition.y
-        return mx > x and mx <= x + w and my > y and my <= y + h
+	local mx = client.mouseScreenPosition.x
+	local my = client.mouseScreenPosition.y
+	return mx > x and mx <= x + w and my > y and my <= y + h
 end
 
 function SaveGUIConfig(xx,yy)
-		local file = io.open(SCRIPT_PATH.."/libs/DRDConfig.txt", "w+")
-        if file then
-                file:write(xx.."\n"..yy)
-                file:close()
-        end
+	local file = io.open(SCRIPT_PATH.."/libs/DRDConfig.txt", "w+")
+	if file then
+			file:write(xx.."\n"..yy)
+			file:close()
+	end
 end
 
 function LoadGUIConfig()
-		local file = io.open(SCRIPT_PATH.."/libs/DRDConfig.txt", "r")
-        if file then
-                xx, yy = file:read("*number", "*number")
-                file:close()
-        end
+	local file = io.open(SCRIPT_PATH.."/libs/DRDConfig.txt", "r")
+	if file then
+			xx, yy = file:read("*number", "*number")
+			file:close()
+	end
 end
 
 function GameClose()
-	for a = 1, 7 do
-			img[a].visible = false
-			rect[a].visible = false
-			text.visible = false
-			spellname[a] = false
-	end
-	effect = {} ranges = {} range = {}
-	spell = {} activated = false move = false
+	text.visible = false
+	spellList = {}
+	spells = {}
+	activated = false 
+	move = false
+	create = true
+	collectgarbage("collect")
 end
 
 script:RegisterEvent(EVENT_CLOSE, GameClose)
 script:RegisterEvent(EVENT_TICK,Tick)
 script:RegisterEvent(EVENT_KEY,Key)
-script:RegisterEvent(EVENT_TICK,Frame)
