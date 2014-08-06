0cb50020571342820ec0b3e8164ef6fd23bd36db
Inactive <SkyIce39@gmail.com>
2014-08-06 14:54:23 +0300

    Update ShowMeMore.lua

--- Scripts/ShowMeMore.lua
+++ Scripts/ShowMeMore.lua
@@ -25,11 +25,11 @@ local TCharge1 = drawMgr:CreateRect(-10,-60,26,26,0xFF8AB160) TCharge1.visible =
 local TCharge2 = drawMgr:CreateRect(0,0,18,18,0xFF8AB160) TCharge2.visible = false
 
 spells = {
--- modifier name, effect name, second effect, aoe-range, spell name
-{"modifier_invoker_sun_strike", "invoker_sun_strike_team","invoker_sun_strike_ring_b","area_of_effect","invoker_sun_strike" },
-{"modifier_lina_light_strike_array", "lina_spell_light_strike_array_ring_collapse","lina_spell_light_strike_array_sphere","light_strike_array_aoe","lina_light_strike_array"},
-{"modifier_kunkka_torrent_thinker", "kunkka_spell_torrent_pool","kunkka_spell_torrent_bubbles_b","radius","kunkka_torrent"},
-{"modifier_leshrac_split_earth_thinker", "leshrac_split_earth_b","leshrac_split_earth_c","radius","leshrac_split_earth"}
+-- modifier name, effect name, second effect, aoe-range
+{"modifier_invoker_sun_strike", "invoker_sun_strike_team","invoker_sun_strike_ring_b",175},
+{"modifier_lina_light_strike_array", "lina_spell_light_strike_array_ring_collapse","lina_spell_light_strike_array_sphere",225},
+{"modifier_kunkka_torrent_thinker", "kunkka_spell_torrent_pool","kunkka_spell_torrent_bubbles_b",225},
+--{"modifier_leshrac_split_earth_thinker", "leshrac_split_earth_b","leshrac_split_earth_c","radius","leshrac_split_earth"}
 }
 
 RangeCastList = {
@@ -140,13 +140,9 @@ function DirectBase(cast,team)
 			for i,k in ipairs(spells) do
 				if modifiers[1].name == k[1] and (not k.handle or k.handle ~= v.handle) then
 					k.handle = v.handle
-					local Spell = FindSpell(v.owner,k[5])
-						if Spell then
-						local Range = GetSpecial(Spell,k[4],Spell.level+0)
-						local entry = { Effect(v, k[2]),Effect(v, k[3]),  Effect( v, "range_display") }
-						entry[3]:SetVector(1, Vector( Range, 0, 0) )
-						table.insert(effects, entry)
-					end
+					local entry = { Effect(v, k[2]),Effect(v, k[3]),  Effect( v, "range_display") }
+					entry[3]:SetVector(1, Vector( k[4], 0, 0) )
+					table.insert(effects, entry)
 				end
 			end
 		end
@@ -397,24 +393,6 @@ function Roha()
 		stage = 1
 		RoshanSideMessage("Respawn","00:00")	
 		script:UnregisterEvent(Roha)
-	end
-end
-
-function GetSpecial(spell,Name,lvl)
-	local specials = spell.specials
-	for _,v in ipairs(specials) do
-		if v.name == Name then
-			return v:GetData( math.min(v.dataCount,lvl) )
-		end
-	end
-end
-
-function FindSpell(target,spellName)
-	local abilities = target.abilities
-	for _,v in ipairs(abilities) do
-		if v.name == spellName then
-			return v
-		end
 	end
 end
 
