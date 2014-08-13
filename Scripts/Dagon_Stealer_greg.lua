c33f7c48226ce4f1b7c1b12067b7f209a2994a61
32103940 <3210@mailinator.com>
2014-08-13 19:22:41 +0800

    Update and rename Dagon_Stealer.lua to Dagon_Stealer_greg.lua

---
+++ Scripts/Dagon_Stealer_greg.lua
@@ -0,0 +1,104 @@
+require("libs.Utils")
+require("libs.ScriptConfig")
+
+config = ScriptConfig.new()
+config:SetParameter("Hotkey", "48", config.TYPE_HOTKEY)
+config:Load()
+
+local key = config.Hotkey
+local xx = client.screenSize.x/300
+local yy = client.screenSize.y/1.55
+
+local activated = true
+local sleep = 250
+local icon = drawMgr:CreateRect(xx,yy,36,24,0x000000ff,drawMgr:GetTextureId("NyanUI/items/dagon")) icon.visible = false
+local rect = drawMgr:CreateRect(xx-1,yy-1,26,25,0xFFFFFF90,true) rect.visible = false
+local dmg = {400,500,600,700,800}
+ 
+--Главная функция
+function Tick(tick)
+ 
+if not client.connected or client.loading or client.console or tick < sleep then return end
+
+sleep = tick + 150
+
+local me = entityList:GetMyHero()
+
+if not me then return end
+       
+local dagon = me:FindDagon()
+local ethereal= me:FindItem("item_ethereal_blade")
+local visible = Draw(activated,dagon)
+local attribute = GetAttribute(me)
+
+rect.visible = visible
+icon.visible = visible
+
+if visible then
+local lvl = string.match (dagon.name, "%d+")
+if not lvl then lvl = 1 end 
+local dmgD = dmg[lvl*1]
+local enemy = entityList:GetEntities({type=LuaEntity.TYPE_HERO,alive=true,visible=true,team = (5-me.team)})
+if not me:IsChanneling() and Nyx(me) then
+for i = 1,#enemy do
+local v = enemy[i]
+if not v:IsIllusion() and dagon.cd==0 then
+if v.health > 0 and GetDistance2D(v,me) < dagon.castRange and v:CanDie() then
+if not v:DoesHaveModifier("modifier_nyx_assassin_spiked_carapace") then
+	
+	if v.health < v:DamageTaken(dmgD, DAMAGE_MAGC, me) then
+	me:SafeCastAbility(dagon,v)
+	
+	elseif v.health < v:DamageTaken(((dmgD+75+2*attribute)*1.4), DAMAGE_MAGC, me) and ethereal and ethereal.cd==0 and dagon.cd==0 then
+	me:SafeCastAbility(ethereal,v)
+	
+	end
+end
+end
+end
+end
+end	
+end
+
+end
+
+function Nyx(target)
+if target.classId == CDOTA_Unit_Hero_Nyx_Assassin and target:DoesHaveModifier("modifier_nyx_assassin_vendetta") then
+return false
+end
+return true
+end
+
+function Draw(one,two)
+if one and two then
+return true
+end
+return false
+end
+
+function Key()
+
+if not client.chat and IsKeyDown(key) then
+activated = not activated
+end
+
+end
+
+function GameClose()
+rect.visible = false
+icon.visible = false	
+end
+
+function GetAttribute(me)
+	local stat = {{me.agility,me.agilityTotal},{me.intellect,me.intellectTotal},{me.strength,me.strengthTotal}}	
+	local abtibute = math.max(stat[1][1],stat[2][1],stat[3][1])
+	for i,v in ipairs(stat) do
+		if abtibute == v[1] then
+			return math.floor(v[2])
+		end
+	end	
+end
+ 
+script:RegisterEvent(EVENT_CLOSE,GameClose)
+script:RegisterEvent(EVENT_KEY,Key)
+script:RegisterEvent(EVENT_TICK,Tick)
