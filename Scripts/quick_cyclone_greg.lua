9229c4c5708220cf1e55bef53942a4169cc5915b
32103940 <3210@mailinator.com>
2014-10-19 03:33:06 +0800

    Update quick_cyclone_greg.lua

--- Scripts/quick_cyclone_greg.lua
+++ Scripts/quick_cyclone_greg.lua
@@ -3,13 +3,17 @@ require("libs.ScriptConfig")
 require("libs.Stuff")
 local config = ScriptConfig.new()
 config:SetParameter("Active", "Q", config.TYPE_HOTKEY)
+config:SetParameter("doubletapInterval", 280)
 config:Load()
 
 local toggleKey   = config.Active
+local doubletapInterval= config.doubletapInterval
 local activ       = false
 local monitor     = client.screenSize.x/1600
 local F14         = drawMgr:CreateFont("F14","Tahoma",14*monitor,550*monitor) 
 local statusText  = drawMgr:CreateText(10*monitor,515*monitor,-1,"(" .. string.char(toggleKey) .. ") Auto Cyclone: Off",F14) statusText.visible = true
+local debugText  = drawMgr:CreateText(10*monitor,485*monitor,-1,"debug",F14) debugText.visible = true
+local start = 0
 
 local hotkeyText
 if string.byte("A") <= toggleKey and toggleKey <= string.byte("Z") then
@@ -20,17 +24,33 @@ end
 
 function Key(msg,code)
 	if client.chat or client.console or client.loading then return end
-	if IsKeyDown(toggleKey) then
-		activ = true
+	
+		if IsKeyDown(toggleKey) and GetTick() < (start+doubletapInterval) and not activ then
+			entityList:GetMyHero():SafeCastItem("item_cyclone",entityList:GetMyHero())
+		end
+		if IsKeyDown(toggleKey) and not activ then
+			start=GetTick()
+			activ = true
 			statusText.text = "(" .. hotkeyText .. ") Auto Cyclone: Active"
-		else
+		end	
+		
+		
+		if code==toggleKey and msg==KEY_UP then 
 			statusText.text = "(" .. hotkeyText .. ") Auto Cyclone: Off"
 			activ=false
+			
+			
 		end
-	end
+		
+end
+
+
+	
 
 function Tick(tick)
-	if not SleepCheck() then return end Sleep(30)
+
+	
+	if not SleepCheck(main) then return end Sleep(30,main)
 	local me    = entityList:GetMyHero()
 	local ID    = me.classId
 	local sheep = me:FindItem("item_cyclone")
@@ -55,16 +75,18 @@ function Disable(me,abilityHex,abilityHexName,sheep)
 		local SI    = v:IsSilenced()
 		local MI    = v:IsMagicImmune()
 		local invis = me:IsInvisible()
-		if not (SI or MI or invis) then
-			if GetDistance2D(v,me) < 800 and sheep and sheep:CanBeCasted() and activ then
-				if activ then
-					me:SafeCastItem("item_cyclone",v)
-					break
-				end
+		if GetDistance2D(v,me) < 800 and not (SI or MI or invis) then
+			if sheep and sheep:CanBeCasted() then
 				if blink and blink.cd > 11 then
 					me:SafeCastItem("item_cyclone",v)
 					break
 				end
+				
+				if activ then
+					me:SafeCastItem("item_cyclone",v)
+					break
+				end
+				
 				if Initiation[v.name] then
 					local eleven = v:FindSpell(Initiation[v.name].Spell)
 					local twelve = eleven.level 
@@ -76,15 +98,17 @@ function Disable(me,abilityHex,abilityHexName,sheep)
 			elseif abilityHex and abilityHexName then
 				local five = me:GetAbility(abilityHex)
 				local six  = me:FindAbility(abilityHexName)
-				if GetDistance2D(v,me) < six.castRange and six and six:CanBeCasted() and activ then
-					if activ then
-						me:SafeCastAbility(five,v)
-						break
-					end
+				if GetDistance2D(v,me) < six.castRange and six and six:CanBeCasted() then
 					if blink and blink.cd > 11 then
 						me:SafeCastAbility(five,v)
 						break
 					end
+					
+					if activ then
+						me:SafeCastAbility(five,v)
+						break
+					end
+					
 					if Initiation[v.name] then
 						local eleven = v:FindSpell(Initiation[v.name].Spell)
 						local twelve = eleven.level 
