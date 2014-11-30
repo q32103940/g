ed268d9a2f3d29a58cbe2c4e1bf03461e87b4934
q32103940 <32103940@mailinator.com>
2014-11-30 15:38:14 +0800

    Update Dagon_Stealer_greg.lua

--- Scripts/Dagon_Stealer_greg.lua
+++ Scripts/Dagon_Stealer_greg.lua
@@ -10,51 +10,165 @@ local xx = client.screenSize.x/300
 local yy = client.screenSize.y/1.55
 
 local activated = true
-local sleep = 250
+local 
+
+sleep = 250
 local icon = drawMgr:CreateRect(xx,yy,36,24,0x000000ff,drawMgr:GetTextureId("NyanUI/items/dagon")) icon.visible = false
-local rect = drawMgr:CreateRect(xx-1,yy-1,26,25,0xFFFFFF90,true) rect.visible = false
+local 
+
+rect = drawMgr:CreateRect(xx-1,yy-1,26,25,0xFFFFFF90,true) rect.visible = false
 local dmg = {400,500,600,700,800}
- 
+
+local monitor     = 
+
+client.screenSize.x/1600
+local F14         = drawMgr:CreateFont("F14","Tahoma",14*monitor,550*monitor) 
+local debugText1  = 
+
+drawMgr:CreateText(10*monitor,475*monitor,-1,"debug1",F14) debugText1.visible = true
+local debugText2  = drawMgr:CreateText
+
+(10*monitor,460*monitor,-1,"debug2",F14) debugText2.visible = true
+local debugText3  = drawMgr:CreateText(10*monitor,445*monitor,-
+
+1,"debug3",F14) debugText3.visible = true 
 --Главная функция
 function Tick(tick)
  
-if not client.connected or client.loading or client.console or tick < sleep then return end
+if not client.connected or 
 
-sleep = tick + 150
+client.loading or 
+
+client.console or tick < sleep then return end
+
+sleep = tick + 10
 
 local me = entityList:GetMyHero()
 
-if not me then return end
+if not me then return 
+
+end
        
-local dagon = me:FindDagon()
+local 
+
+dagon = me:FindDagon()
 local ethereal= me:FindItem("item_ethereal_blade")
 local visible = Draw(activated,dagon)
-local attribute = GetAttribute(me)
+local attribute = 
 
-rect.visible = visible
+GetAttribute(me)
+local purify = me:FindSpell("oracle_purifying_flames")
+local fortune= me:FindSpell("oracle_fortunes_end")
+
+rect.visible = 
+
+visible
 icon.visible = visible
 
-if visible then
+if visible then 
 local lvl = string.match (dagon.name, "%d+")
 if not lvl then lvl = 1 end 
-local dmgD = dmg[lvl*1]
+dmgD = dmg[lvl*1]
+else dmgD=0
+end
+
+
 local enemy = entityList:GetEntities({type=LuaEntity.TYPE_HERO,alive=true,visible=true,team = (5-me.team)})
-if not me:IsChanneling() and Nyx(me) then
-for i = 1,#enemy do
+
+
+for i = 
+
+1,#enemy do
 local v = enemy[i]
-if not v:IsIllusion() and dagon.cd==0 then
-if v.health > 0 and GetDistance2D(v,me) < dagon.castRange and v:CanDie() then
-if not v:DoesHaveModifier("modifier_nyx_assassin_spiked_carapace") then
-	
-	if v.health < v:DamageTaken(dmgD, DAMAGE_MAGC, me) then
-	me:SafeCastAbility(dagon,v)
-	
-	elseif v.health < v:DamageTaken(((dmgD+75+2*attribute)*1.4), DAMAGE_MAGC, me) and ethereal and ethereal.cd==0 and dagon.cd==0 then
-	me:SafeCastAbility(ethereal,v)
-	
+if not v:IsIllusion() then
+if v.health > 0 and v:CanDie() and GetDistance2D(v,me) < 1000  then
+if not 
+
+v:DoesHaveModifier("modifier_nyx_assassin_spiked_carapace") then
+
+--for fortune's end channel detection (debug)
+relativeangle=me:FindRelativeAngle(v)
+debugText2.text=""..relativeangle
+--calculates raw dmg from fortunes end if you released the 
+
+channeling
+if fortune and me:IsChanneling(fortune) and me:FindRelativeAngle(v)<0.05  then 
+fortundemg=fortune.level*75
+else fortunedmg=0
+end
+debugText1.text=""..fortunedmg
+
+--calculates raw dmg from purify flames if you could cast it on the current target
+if purify and 
+
+purify.cd==0 and GetDistance2D(v,me) < purify.castRange  then
+purifydmg=purify.level*90
+else purifydmg=0
+end
+debugText3.text=""..purifydmg
+
+--dagon only
+if dagon and dagon.cd==0 and GetDistance2D(v,me) < dagon.castRange and v.health < v:DamageTaken(dmgD, DAMAGE_MAGC, me) 
+
+then
+me:SafeCastAbility(dagon,v, false)
+
+--fortunes + purify
+elseif v.health < v:DamageTaken(fortunedmg+purifydmg, DAMAGE_MAGC, me) then
+me:SafeCastAbility(purify,v, false)
+
+--fortunes + dagon
+elseif dagon and dagon.cd==0 and GetDistance2D(v,me) < 
+dagon.castRange and 
+
+relativeangle<0.05 and v.health < v:DamageTaken(dmgD+fortunedmg, DAMAGE_MAGC, me) then
+me:SafeCastAbility(dagon,v, false)
+
+--fortunes + 
+
+purify + dagon
+elseif dagon and dagon.cd==0 and GetDistance2D(v,me) < dagon.castRange and GetDistance2D(v,me) < purify.castRange and 
+
+relativeangle<0.05 and v.health < v:DamageTaken(dmgD+fortunedmg+purifydmg, DAMAGE_MAGC,me) then
+me:SafeCastAbility(dagon,v, false)
+
+
+--[[
+elseif dagon.cd==0 and purify and purify.cd==0 and 
+
+v.health  < v:DamageTaken(dmgD+90*purify.level, DAMAGE_MAGC, me) then
+me:SafeCastAbility(dagon,v)
+
+elseif ethereal and 
+
+ethereal.cd==0 and purify and purify.cd==0 and v.health < v:DamageTaken
+
+(((90*purify.level+75+2*attribute)*1.4), DAMAGE_MAGC, me)  then
+
+
+me:SafeCastAbility(ethereal,v)
+
+elseif ethereal and ethereal.cd==0 and 
+
+dagon.cd==0 and v.health < v:DamageTaken(((dmgD
+
++75+2*attribute)*1.4), DAMAGE_MAGC, me)  then
+me:SafeCastAbility(ethereal,v)
+
+elseif 
+
+ethereal and ethereal.cd==0 and 
+
+dagon.cd==0 and purify and purify.cd==0 and v.health < v:DamageTaken(((dmgD+75+2*attribute
+
++90*purify.level)*1.4), DAMAGE_MAGC, me) then
+
+
+me:SafeCastAbility(ethereal,v)
+--]]
 	end
-end
-end
+        
+
 end
 end
 end	
@@ -62,43 +176,43 @@ end
 
 end
 
-function Nyx(target)
-if target.classId == CDOTA_Unit_Hero_Nyx_Assassin and target:DoesHaveModifier("modifier_nyx_assassin_vendetta") then
-return false
-end
-return true
-end
 
 function Draw(one,two)
-if one and two then
+if 
+
+one and two then
 return true
 end
-return false
+return 
+
+false
 end
 
-function Key()
-
-if not client.chat and IsKeyDown(key) then
-activated = not activated
-end
-
-end
 
 function GameClose()
 rect.visible = false
 icon.visible = false	
 end
 
-function GetAttribute(me)
-	local stat = {{me.agility,me.agilityTotal},{me.intellect,me.intellectTotal},{me.strength,me.strengthTotal}}	
-	local abtibute = math.max(stat[1][1],stat[2][1],stat[3][1])
+function 
+
+GetAttribute(me)
+	local stat = 
+
+{{me.agility,me.agilityTotal},{me.intellect,me.intellectTotal},{me.strength,me.strengthTotal}}	
+	local abtibute = math.max(stat
+
+[1][1],stat[2][1],stat[3][1])
 	for i,v in ipairs(stat) do
-		if abtibute == v[1] then
-			return math.floor(v[2])
+		if abtibute == v[1] 
+
+then
+			return 
+
+math.floor(v[2])
 		end
 	end	
 end
  
 script:RegisterEvent(EVENT_CLOSE,GameClose)
-script:RegisterEvent(EVENT_KEY,Key)
 script:RegisterEvent(EVENT_TICK,Tick)
